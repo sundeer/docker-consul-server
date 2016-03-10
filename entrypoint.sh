dc6a553d1342a069f6d4e2b95c26a38556d607a7
sundeer <sundeer@rhsjmm.com>
2016-03-10 17:20:18 +0000

    clean up

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,36 +6,6 @@ set -e
 # wouldn't do either of these functions so we'd leak zombies as well as do
 # unclean termination of all our sub-processes.
 
-# You can set CONSUL_BIND_INTERFACE to the name of the interface you'd like to
-# bind to and this will look up the IP and pass the proper -bind= option along
-# to Consul.
-# CONSUL_BIND=
-# if [ -n "$CONSUL_BIND_INTERFACE" ]; then
-#   CONSUL_BIND_ADDRESS=$(ip -o -4 addr list $CONSUL_BIND_INTERFACE | awk '{print $4}' | cut -d/ -f1)
-#   if [ -z "$CONSUL_BIND_ADDRESS" ]; then
-#     echo "Could not find IP for interface '$CONSUL_BIND_INTERFACE', exiting"
-#     exit 1
-#   fi
-#
-#   CONSUL_BIND="-bind=$CONSUL_BIND_ADDRESS"
-#   echo "==> Found address '$CONSUL_BIND_ADDRESS' for interface '$CONSUL_BIND_INTERFACE', setting bind option..."
-# fi
-#
-# # You can set CONSUL_CLIENT_INTERFACE to the name of the interface you'd like to
-# # bind client intefaces (HTTP, DNS, and RPC) to and this will look up the IP and
-# # pass the proper -client= option along to Consul.
-# CONSUL_CLIENT=
-# if [ -n "$CONSUL_CLIENT_INTERFACE" ]; then
-#   CONSUL_CLIENT_ADDRESS=$(ip -o -4 addr list $CONSUL_CLIENT_INTERFACE | awk '{print $4}' | cut -d/ -f1)
-#   if [ -z "$CONSUL_CLIENT_ADDRESS" ]; then
-#     echo "Could not find IP for interface '$CONSUL_CLIENT_INTERFACE', exiting"
-#     exit 1
-#   fi
-#
-#   CONSUL_CLIENT="-client=$CONSUL_CLIENT_ADDRESS"
-#   echo "==> Found address '$CONSUL_CLIENT_ADDRESS' for interface '$CONSUL_CLIENT_INTERFACE', setting client option..."
-# fi
-
 # This exposes three different modes, and allows for the execution of arbitrary
 # commands if one of these modes isn't chosen. Each of the modes will read from
 # the config directory, allowing for easy customization by placing JSON files
